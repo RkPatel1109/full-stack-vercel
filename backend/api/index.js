6eef6dc47b0853c78e8e2748a53bf679d8c34674
RkPatel1109 <patel.rk11190@gmail.com>
2025-07-26 15:46:54 +0000

    add user API endpoints for fetching and creating users

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const { PrismaClient } = require('./generated/client');
+const prisma = new PrismaClient();
+
 const app = express();
 const port = 5000;
 const cors = require("cors")
@@ -13,6 +16,18 @@ app.get('/api/hello', (req, res) => {
 
 app.get('/', (req, res) => {
   res.json({ message: 'Server is Running' });
+});
+app.get('/api/users', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.post('/api/users', async (req, res) => {
+  const { email, name } = req.body;
+  const newUser = await prisma.user.create({
+    data: { email, name },
+  });
+  res.json(newUser);
 });
 
 // Start the server
